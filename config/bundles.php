804b28fc3bdae0fc149c441d2f8a9e585d676637
Mediashare <mediashare.supp@gmail.com>
2019-12-29 08:43:30 +0100

    admin

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
